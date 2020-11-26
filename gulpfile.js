62284af0ae9035c993912fd2c9406a715138c39d
_dev <junior_dev@Mac-mini-developer.local>
2020-11-26 13:26:29 +0200

    some changes

--- gulpfile.js
+++ gulpfile.js
@@ -49,6 +49,7 @@ const scss = require('gulp-sass');
 const uglify = require('gulp-uglify-es').default;
 const htmlBeautify = require('gulp-html-beautify');
 const pug = require('gulp-pug');
+const handlebars = require('gulp-compile-handlebars');
 const notify = require('gulp-notify');
 
 const browserSyncTask = () => {
@@ -80,6 +81,49 @@ const htmlTask = () => {
     .pipe(browserSync.stream())
     .pipe(notify(`${projectFolder} started!`))
 };
+
+// const handlebarsConfig = target => {
+//   const handlebarsOptions = {
+//     ignorePartials: true,
+//     batch: [`${srcFolder}/${target}`],
+//     helpers: {
+//       repeat(num, el) {
+//         let acc = '';
+
+//         for (let i = 0; i < num; i++) {
+//           acc += el.fn(i);
+//         }
+
+//         return acc;
+//       }
+//     }
+//   };
+
+//   return handlebarsOptions;
+// }
+
+// const handlebarsLayout = handlebarsConfig('layout');
+// const handlebarsComponents = handlebarsConfig('components');
+
+// const handlebarsData = {
+//   firstName: 'Ross'
+// };
+
+// const htmlTask = () => {
+//   return src(path.src.html)
+//     .pipe(fileInclude())
+//     .pipe(handlebars(handlebarsData, handlebarsLayout))
+//     .pipe(handlebars(handlebarsData, handlebarsComponents))
+//     .pipe(htmlBeautify(htmlBeautifyConfig))
+//     .pipe(
+//       rename({
+//         extname: '.html'
+//       })
+//     )
+//     .pipe(dest(path.dist.html))
+//     .pipe(browserSync.stream())
+//     .pipe(notify(`${projectFolder} started!`))
+// };
 
 const cssTask = () => {
   return src(path.src.css)
